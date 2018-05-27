c72e27455fadadf757ca05298455e6183a51cfd2
cgott <camerongott@hotmail.com>
2018-05-27 13:55:33 -0700

    Enemy spawner
    First-pass prototype of enemy spawner object. Successfully spawns 4 skeleton enemies in a '+' pattern around the spawn point.

---
+++ RPGDemo/Unity end file/Assets/Scripts/EnemySpawner_script.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner_script : MonoBehaviour {
+
+    public Enemy enemyPrefabToSpawn;
+    public bool spawnAtGameStart;
+    public int maxEnemiesSpawnable;
+    public Transform spawnerGFX;
+
+	// Use this for initialization
+	void Start () {
+
+		if(spawnAtGameStart)
+        {
+            spawnFourEnemies();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    bool spawnEnemyAtPoint(Vector3 spawnPoint, Quaternion spawnRotation)
+    {
+        bool successfulSpawn = false;
+        Enemy tempcpy = Instantiate(enemyPrefabToSpawn, spawnPoint, spawnRotation);
+        if(tempcpy)
+        {
+            successfulSpawn = true;
+        }
+        return successfulSpawn;
+    }
+
+    //spawns four Enemy objects in a '+' pattern centered on the enemyspawner gfx location
+    bool spawnFourEnemies()
+    {
+        bool spawnSuccess = false;
+        bool allSpawnsSuccessful = true;
+        Vector3 north = spawnerGFX.position;
+        Vector3 south = spawnerGFX.position;
+        Vector3 east = spawnerGFX.position;
+        Vector3 west = spawnerGFX.position;
+
+        north.x += 1;
+        south.x -= 1;
+        east.z += 1;
+        west.z -= 1;
+
+        spawnSuccess = spawnEnemyAtPoint(north, spawnerGFX.rotation);
+        if(spawnSuccess == false)
+        {
+            allSpawnsSuccessful = false;
+        }
+        spawnSuccess = spawnEnemyAtPoint(south, spawnerGFX.rotation);
+        if (spawnSuccess == false)
+        {
+            allSpawnsSuccessful = false;
+        }
+        spawnSuccess = spawnEnemyAtPoint(east, spawnerGFX.rotation);
+        if (spawnSuccess == false)
+        {
+            allSpawnsSuccessful = false;
+        }
+        spawnSuccess = spawnEnemyAtPoint(west, spawnerGFX.rotation);
+        if (spawnSuccess == false)
+        {
+            allSpawnsSuccessful = false;
+        }
+        return allSpawnsSuccessful;
+    }
+}
